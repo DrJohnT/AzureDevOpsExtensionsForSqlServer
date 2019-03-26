819651cc3ee4a092922a6250ab1eef7771222872
Dr John Tunnicliffe <john@bovi.co.uk>
2019-03-26 18:43:10 +0000

    Updated publisher

--- extensions/TFX-Misc.ps1
+++
@@ -1,4 +0,0 @@
-## uses templates which are far too complicated
-#tfx extension init --path .\DeployDatabase --publisher DrJohnT --extension-name DeployDatabase --extension-id c3a3d7e3-20b9-4821-bb4c-fe7f222c1af4
-
-#tfx extension publisher create --publisher DrJohnT --display-name "Dr. John Tunnicliffe" --auth-type pat --token ??? --trace-level debug --decription "Dr. John Tunnicliffe, BI Architect specializing in Microsoft Business Intelligence products"
---
+++ tests/README.md
@@ -0,0 +1,4 @@
+## How to Run Tests
+
+* Install Pester. You can do by `Install-Module Pester`
+* Goto the respective tests folder and run `Invoke-Pester` or run in top level folder to run all tests on all extensions
