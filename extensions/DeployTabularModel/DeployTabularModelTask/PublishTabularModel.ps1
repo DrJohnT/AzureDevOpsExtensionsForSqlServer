1b019254ac10f07e6dde55710a74f424df5589d0
Dr John Tunnicliffe <john@bovi.co.uk>
2019-04-18 08:08:37 +0100

    Updated documentation

--- extensions/DeployTabularModel/DeployTabularModelTask/PublishTabularModel.ps1
+++ extensions/DeployTabularModel/DeployTabularModelTask/PublishTabularModel.ps1
@@ -3,44 +3,45 @@ param()
 
 <#
 	.SYNOPSIS
-    Publishes a SQL Server Database DacPac using a DacPac publish profile
+    Publishes a SQL Server SSAS Tabular Model (AsDatabase file) using Microsoft.AnalysisServices.Deployment.exe
 
 	.DESCRIPTION
-    Publishes a SSDT DacPac using a specified DAC publish profile from your solution.
-    Basically deploys the DACPAC by invoking SqlPackage.exe using a DacPac Publish profile
+    Publishes a AsDatabase file built by your solution.
+    Basically deploys the AsDatabase by invoking Microsoft.AnalysisServices.Deployment.exe
 
     Script written by (c) Dr. John Tunnicliffe, 2019 https://github.com/DrJohnT/AzureDevOpsExtensionsForSqlServer
 	This PowerShell script is released under the MIT license http://www.opensource.org/licenses/MIT
 
-    Depends on PowerShell module PublishDacPac written by (c) Dr. John Tunnicliffe, 2019 https://github.com/DrJohnT/PublishDacPac
+    Depends on PowerShell module PublishDacPac written by (c) Dr. John Tunnicliffe, 2019 https://github.com/DrJohnT/DeployCube
 #>
 
     $ModulePath = Split-Path -Parent $MyInvocation.MyCommand.Path;
     $ModulePath = Resolve-Path "$ModulePath\ps_modules\DeployCube\DeployCube.psd1";
     import-Module -Name $ModulePath;
+
     Write-Host "Reading inputs";
 
-    [string]$DacPacPath = Get-VstsInput -Name "DacPacPath" -Require;
-    [string]$DacPublishProfile = Get-VstsInput -Name "DacPublishProfile" -Require;
-    [string]$TargetServerName = Get-VstsInput -Name  "TargetServerName" -Require;
-    [string]$TargetDatabaseName = Get-VstsInput -Name "TargetDatabaseName";
+    [string]$AsDatabasePath = Get-VstsInput -Name "AsDatabasePath" -Require;
+    [string]$AsServer = Get-VstsInput -Name  "AsServer" -Require;
+    [string]$CubeDatabaseName = Get-VstsInput -Name "CubeDatabaseName";
     [string]$PreferredVersion = Get-VstsInput -Name "PreferredVersion";
+    [string]$ProcessingOption = Get-VstsInput -Name "ProcessingOption";
 
     $global:ErrorActionPreference = 'Stop';
 
     Trace-VstsEnteringInvocation $MyInvocation;
 
-    Write-Host "Invoking Publish-DacPac (https://github.com/DrJohnT/PublishDacPac) with the following parameters:";
-    Write-Host "DacPacPath:         $DacPacPath";
-    Write-Host "DacPublishProfile:  $DacPublishProfile";
-    Write-Host "TargetServerName:   $TargetServerName";
-    Write-Host "TargetDatabaseName: $TargetDatabaseName"
+    Write-Host "Invoking Publish-Cube (https://github.com/DrJohnT/DeployCube) with the following parameters:";
+    Write-Host "AsDatabasePath:     $AsDatabasePath";
+    Write-Host "AsServer:           $AsServer";
+    Write-Host "CubeDatabaseName:   $CubeDatabaseName"
     Write-Host "PreferredVersion:   $PreferredVersion"
+    Write-Host "ProcessingOption:   $ProcessingOption"
 
     Write-Host "==============================================================================";
 
     try {
-        Publish-DacPac -DacPacPath $DacPacPath -DacPublishProfile $DacPublishProfile -TargetServerName $TargetServerName -TargetDatabaseName $TargetDatabaseName -PreferredVersion $PreferredVersion;
+        Publish-Cube -AsDatabasePath $AsDatabasePath -Server $AsServer -CubeDatabase $CubeDatabaseName -PreferredVersion $PreferredVersion -ProcessingOption $ProcessingOption;
     } finally {
         Write-Host "==============================================================================";
         Trace-VstsLeavingInvocation $MyInvocation
