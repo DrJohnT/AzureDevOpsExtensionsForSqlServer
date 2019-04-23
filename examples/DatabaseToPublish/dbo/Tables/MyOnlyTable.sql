6f81a48c6d8dc1b1e2aaa4b4936bddec4170570d
DrJohnT <john@bovi.co.uk>
2019-04-23 15:48:17 +0100

    Updated example solution

---
+++ examples/DatabaseToPublish/dbo/Tables/MyOnlyTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE dbo.MyOnlyTable
+(
+	MyOnlyTableId INT NOT NULL  identity(1,1),
+	MyOnlyColumn nvarchar(100) not null,	
+	MyOnlyValue money not null, 
+	InMyCurrency char(3) not null,
+    CONSTRAINT PK_MyOnlyTable PRIMARY KEY (MyOnlyTableId),  -- always name your PKs!
+)
