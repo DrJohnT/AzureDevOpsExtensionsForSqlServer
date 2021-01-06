ee207572a6fea77541b9bd71d7871f3d9ac81a17
Dr John Tunnicliffe <john@bovi.co.uk>
2021-01-06 23:34:46 +0000

    Upgraded VSIX packaging scripts to check versions

--- extensions/VsixPackagePublishDacPac.ps1
+++ extensions/VsixPackagePublishDacPac.ps1
@@ -4,10 +4,38 @@
 # npm install -g npm
 # npm install -g tfx-cli
 Remove-Item DrJohnExtensions.PublishDacPac*.vsix;
+# build subordinate package DeployDatabase at same time
+Remove-Item DrJohnExtensions.DeployDatabase*.vsix;
 
 $CurrentFolder = Split-Path -Parent $MyInvocation.MyCommand.Path;
+$relatedPackages = ("PublishDacPac", "DeployDatabase");
+foreach ($packageFolder in $relatedPackages) {
+    $folderName =  Resolve-Path "$CurrentFolder\$packageFolder";
 
-$folderName =  Resolve-Path "$CurrentFolder\PublishDacPac";
+    $vssFile = Resolve-Path -Path "$folderName\vss-extension.json";
+    $json = Get-Content -Raw -Path $vssFile | ConvertFrom-Json;
+    $PackageName = $json.id;
+    $CurrentVersion = $json.version;
+    Write-Host "Package: $PackageName" -ForegroundColor Blue; 
+    Write-Host "Current Version: $CurrentVersion" -ForegroundColor Yellow; 
 
-tfx extension create --manifests vss-extension.json --root $folderName --rev-version --trace-level debug;
+    [System.IO.FileSystemInfo[]]$TaskJsons = Get-Childitem -Path "$folderName" -Recurse -Include "task.json" -ErrorAction SilentlyContinue;
+    foreach ($TaskJson in $TaskJsons) {
+        $json = Get-Content -Raw -Path $TaskJson | ConvertFrom-Json;
+        $TaskName = $json.name;
+        $TaskMajor = $json.version.Major;
+        $TaskMinor = $json.version.Minor;
+        $TaskPatch = $json.version.Patch;
+        Write-Host "  $TaskName    $TaskMajor.$TaskMinor.$TaskPatch" -ForegroundColor Yellow; 
+    }
+}
 
+$confirmation = Read-Host "Are you Sure?  Type Y to Proceed."
+if ($confirmation -eq 'Y') {
+    foreach ($packageFolder in $relatedPackages) {
+        $folderName =  Resolve-Path "$CurrentFolder\$packageFolder";
+    
+        tfx extension create --manifests vss-extension.json --root $folderName --rev-version; 
+        #--trace-level debug;
+    }    
+}
--- extensions/VsixPackageRunSqlCmdScripts.ps1
+++ extensions/VsixPackageRunSqlCmdScripts.ps1
@@ -8,5 +8,29 @@ $CurrentFolder = Split-Path -Parent $MyInvocation.MyCommand.Path;
 
 $folderName =  Resolve-Path "$CurrentFolder\RunSqlCmdScripts";
 
-tfx extension create --manifests vss-extension.json --root $folderName --rev-version --trace-level debug;
+$vssFile = Resolve-Path -Path "$folderName\vss-extension.json";
+$json = Get-Content -Raw -Path $vssFile | ConvertFrom-Json;
+$PackageName = $json.id;
+$CurrentVersion = $json.version;
+Write-Host "Package: $PackageName" -ForegroundColor Blue; 
+Write-Host "Current Version: $CurrentVersion" -ForegroundColor Yellow; 
+
+[System.IO.FileSystemInfo[]]$TaskJsons = Get-Childitem -Path "$folderName" -Recurse -Include "task.json" -ErrorAction SilentlyContinue;
+foreach ($TaskJson in $TaskJsons) {
+    $json = Get-Content -Raw -Path $TaskJson | ConvertFrom-Json;
+    $TaskName = $json.name;
+    $TaskMajor = $json.version.Major;
+    $TaskMinor = $json.version.Minor;
+    $TaskPatch = $json.version.Patch;
+    Write-Host "  $TaskName    $TaskMajor.$TaskMinor.$TaskPatch" -ForegroundColor Yellow; 
+}
+
+$confirmation = Read-Host "Are you Sure?  Type Y to Proceed."
+if ($confirmation -eq 'Y') {
+    # proceed
+    #Write-Host "Add logic to check task.json version numbers"
+    tfx extension create --manifests vss-extension.json --root $folderName --rev-version; 
+    #--trace-level debug;
+}
+
 
