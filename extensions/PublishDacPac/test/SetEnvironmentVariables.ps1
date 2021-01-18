83c6e88944ca86938aae15c723c6b4d9a0ad97f5
Dr John Tunnicliffe <john@bovi.co.uk>
2021-01-18 12:22:45 +0000

    Added SetEnv script

---
+++ extensions/PublishDacPac/test/SetEnvironmentVariables.ps1
@@ -0,0 +1,10 @@
+# Set environment variables for tests
+$Env:AzureServerInstance   = "$(AzureServerInstance)"; 
+$Env:AzureDatabase = "$(AzureDatabase)";
+$Env:AzureAuthenticationUser   = "$(AzureAuthenticationUser)"; 
+$Env:AzureAuthenticationPassword = "$(AzureAuthenticationPassword)";
+
+$Env:ServerInstance   = "$(ServerInstance)"; 
+$Env:Database = "$(Database)";
+$Env:AuthenticationUser   = "$(AuthenticationUser)"; 
+$Env:AuthenticationPassword = "$(AuthenticationPassword)";
